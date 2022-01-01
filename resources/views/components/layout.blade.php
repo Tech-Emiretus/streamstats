44c673cf6df2304af8d022fc1b5056113335db04
Gideon Appoh <gideonappoh@gmail.com>
2022-01-01 07:51:42 +0000

    Add favicon

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,6 +7,8 @@
         <title>{{ $title . ' | StreamStats' }}</title>
 
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link rel="icon" type="image/png" sizes="32x32" href="https://static.twitchcdn.net/assets/favicon-32-e29e246c157142c94346.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="https://static.twitchcdn.net/assets/favicon-16-52e571ffea063af7a7f4.png">
     </head>
 
     <body class="bg-slate-100 text-black h-full">
